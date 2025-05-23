afdf9315f54660aa995df4b8bfaad61425ca96c2
James Chen <jameschan312.cn@gmail.com>
2025-05-23 18:35:46 -0400

    Start refactoring

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { HourMinuteSecond } from './HourMinuteSecond.js'
-import { SlowHourMinuteSecond } from './SlowHourMinuteSecond.js'
-
-export * from './HourMinuteSecond.js'
-export * from './QuickHourMinuteSecond.js'
-export * from './SlowHourMinuteSecond.js'
-
-HourMinuteSecond.setStaticInitiateClass(SlowHourMinuteSecond)
